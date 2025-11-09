4f3fc61159997c605b81dc0181020629b22be9cf
Flames <vibe-coder@flames.com>
2025-11-09 10:03:19 +0000

    Update project prj_wi9znt95 - 2025-11-09T10:03:19.292Z

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Features from './components/Features';
-import CTA from './components/CTA';
 import Footer from './components/Footer';
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
       <main>
         <Hero />
         <Features />
-        <CTA />
       </main>
       <Footer />
     </div>
